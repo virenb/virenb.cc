fe3f7aab5e2ae26ced6b57087ef8640f92fb1fe4
virenb <virenb@gmail.com>
2020-08-21 13:27:34 -0400

    Update info on index

--- components/layout.js
+++ components/layout.js
@@ -3,8 +3,8 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = 'Your Name';
-export const siteTitle = 'Next.js Sample Website';
+const name = 'Viren Bhagat';
+export const siteTitle = 'Viren Bhagat';
 
 export default function Layout({ children, home }) {
   return (

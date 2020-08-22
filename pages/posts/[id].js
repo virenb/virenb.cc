9ef9f63c0400faf5771c99eaa43865e3f91b4bbe
virenb <virenb@gmail.com>
2020-08-22 16:18:26 -0400

    Clean up index.js

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -2,6 +2,8 @@ import Head from 'next/head';
 import Layout from '../../components/layout';
 import Date from '../../components/date';
 
+import utilStyles from '../../styles/utils.module.css';
+
 import { getAllPostIds, getPostData } from '../../lib/posts';
 
 export default function Post({ postData }) {
@@ -10,12 +12,13 @@ export default function Post({ postData }) {
       <Head>
         <title>{postData.title}</title>
       </Head>
-      <br />
-      {postData.id}
-      <br />
-      <Date dateString={postData.date} />
-      <br />
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   );
 }
